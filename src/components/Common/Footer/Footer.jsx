42cdbcc8d5fe3ab8da33b8d5aff239d2eb4fc625
mohit agarwal <mohit724agarwal@gmail.com>
2023-05-04 02:11:30 +0530

    Footer Updated

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div className='footer flex-justify-center'>Footer</div>
+    <div className='footer flex-justify-center'>
+      <BsGithub id='footer-icon'/>
+      <BsLinkedin id='footer-icon'/>
+      <BsTwitter id='footer-icon'/>
+      <BsFacebook id='footer-icon'/>
+      <BsInstagram id='footer-icon'/>
+    </div>
   )
 }
 
